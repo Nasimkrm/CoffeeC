b45fef56feecb99ecc8154e4a2bf60af8b9d5980
Nasimkrm <nassim.karimi.nk@gmail.com>
2022-07-30 13:37:17 +0200

    Changes to Procfile and app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+//jshint esversion:6
+
 const express = require('express')
 const app = express()
 app.use(express.static('public'));
-
+const port = 3000
 
 app.get('/', (req, res) => {
   res.sendFile("index.html", {root:__dirname})
@@ -10,7 +12,7 @@ app.get('/', (req, res) => {
 
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Example app listening on `+port)
 });
